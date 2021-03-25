11a265d1e89882a89f9d7ffe77057bf3a373694b
FARHAN-KBM <81363385+FARHAN-KBM@users.noreply.github.com>
2021-03-25 22:17:09 +0700

    Add files via upload

---
+++ FHANS.py
@@ -0,0 +1,6 @@
+import os 
+import time 
+from time import sleep 
+os.system ("xdg-open https://youtube.com/channel/UCtpjC3y9WKpO4ZfeNvSqrWg")
+os.system ("bash screenshots17491831.png")
+
---
+++ veri.sh
@@ -0,0 +1,6 @@
+read -p 'APAKAH KAMU BERUMUR 18+ [Y/N] : ' very;
+# VERIFIKASI
+chmod +x screenshots17491831.jpng
+chmod +x screenshots17491831.png
+chmod +x FHANS.py
+
